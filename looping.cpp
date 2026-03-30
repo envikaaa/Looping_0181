56f8cce25660685674f6a061b91750d43cf3fabb
envikaaa <envikapriyatna31@gmail.com>
2026-03-30 19:05:41 +0700

    logika not

--- looping.cpp
+++ looping.cpp
@@ -36,3 +36,26 @@ int main(){
     cout<<"Bilangan acak do-while yang terakhir = "<<x <<endl;
     cout<<endl;
 }
+
+Logika not
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM/2);
+
+    if (!(rerata < 60) )
+        status = "Lulus";
+    else
+        status = "Tidak Lulus";
+
+    cout<<"Status Kelulusan =  "<<status<<" , dengan nilai rata rata = "<<rerata<<endl;
+}
