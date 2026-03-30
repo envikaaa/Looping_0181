8ddd2dc481d9e2b014968616053ce30abf7aac07
envikaaa <envikapriyatna31@gmail.com>
2026-03-30 19:59:44 +0700

    prosedur

--- switchCase.cpp
+++ switchCase.cpp
@@ -37,3 +37,19 @@ void pilihanJurusan() {
         statusKuota = "Error: Kode Tidak Valid";
     }
 }
+
+//prosedur
+void input() {
+     cout << "\n=== HASIL SELEKSI ===" << endl;
+     cout << "Jurusan Pilihan : " << namaJurusan << endl;
+     cout << "Status Kuota    : " << namaJurusan << endl;
+     cout << "----------------------------------" << endl;
+}
+
+int main() {
+    input():
+    pilihanJurusan();
+    output();
+
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,23 @@
+int main(){
+    int x;
+
+    //perulangan dengan FOR
+    //mencetak tulisasn sebanyak 5 kali
+    cout<<"PERULANGAN FOR"<<endl
+    for(int i=0; i<=4; i++{
+        cout<<"Teknologi Informasi UMY"<<endl;
+    })
+    cout<<endl;
+    //untuk mendapatkan urutan angka acak yg berbeda
+    srand(time(0));
+
+    //perulangan dengan WHILE
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }
+    cout<<"Bilangan acak while yang terakhir = "<<x <<endl;
+    cout<<endl;
